34309be235bb5a1262c84870b791af268099bf16
ErwinGonzalezBonilla <157295531+ErwinGonzalezBonilla@users.noreply.github.com>
2025-08-31 12:54:24 +0000

    lineamiento de codigo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
+function generarExcusa() {
+    let who =['El perro', 'Mi abuela','El cartero','Mi pájaro'];
+    let action =['se comió', 'orinó en', 'aplastó','rompió'];
+    let what =['mi tarea', 'mi teléfono','el coche'];
+    let when =['antes de la clase', 'cuando estaba durmiendo', 'mientras hacía ejercicio','durante mi almuerzo','mientras estaba rezando'];
+    let subject =who[randomNumberFromArray(who)];
+    let verb =action[randomNumberFromArray(action)];
+    let object = what[randomNumberFromArray(what)];
+    let moment = when[randomNumberFromArray(when)];
 
+    let excusa = `${subject} ${verb} ${object} ${moment}.`;
+    
+    document.getElementById("excusa").innerText = excusa;
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function randomNumberFromArray(array){
+    let randomNumber = Math.floor(Math.random() * array.length) 
+    return randomNumber
+}   
+window.onload = generarExcusa();
+ 
